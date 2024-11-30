a819921ab43f67f971e9bab23c501da5be6aef83
bam <juhyun19981129@gmail.com>
2024-11-30 17:59:24 +0900

    first commit

---
+++ src/main/java/com/joo/abysshop/AbysshopApplication.java
@@ -0,0 +1,13 @@
+package com.joo.abysshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbysshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbysshopApplication.class, args);
+	}
+
+}

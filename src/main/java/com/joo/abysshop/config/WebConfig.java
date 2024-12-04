bda22f0e3ed643bba3f0f1e5823cc3684eb4c5d1
bam <juhyun19981129@gmail.com>
2024-12-04 16:15:39 +0900

    webapp 설정을 위한 Webconfig 수정

--- src/main/java/com/joo/abysshop/config/WebConfig.java
+++ src/main/java/com/joo/abysshop/config/WebConfig.java
@@ -2,7 +2,9 @@ package com.joo.abysshop.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -10,5 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
             .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        registry.viewResolver(resolver);
     }
 }

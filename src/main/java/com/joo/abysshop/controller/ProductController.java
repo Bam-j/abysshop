eb2aa526bb35f2ae1f4ffecc380a5d3c72598bd2
bam <juhyun19981129@gmail.com>
2024-12-29 15:17:50 +0900

    product 관련 요청 컨트롤러 작성

--- src/main/java/com/joo/abysshop/controller/ProductController.java
+++ src/main/java/com/joo/abysshop/controller/ProductController.java
@@ -1,19 +1,29 @@
 package com.joo.abysshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ProductController {
 
-    @GetMapping("/product/product-purchase")
-    public String productPurchase() {
-        return "product/productPurchases";
+    @GetMapping("/product/list/ptoducts")
+    public String getProductList() {
+        //TODO: products_table에서 type이 'product'인 상품들만 뽑아와서 화면에 전달
+        return "";
     }
 
-    @GetMapping("/product/point-purchase")
-    public String pointPurchase() {
-        return "product/pointPurchase";
+    @GetMapping("/product/list/points")
+    public String getPointList() {
+        //TODO: products_table에서 type이 'points'인 상품들만 뽑아와서 화면에 전달
+        return "";
+    }
+
+    @GetMapping("/product/detail/{id}")
+    public String getProductDetail(@PathVariable int id, Model model) {
+        //TODO: 상품을 찾아서 detail 페이지로 이동
+        return "product/detail";
     }
 
     /*

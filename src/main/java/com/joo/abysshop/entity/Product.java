55033cb8bb202292edceecb9a76fcb41ef1589a2
bam <juhyun19981129@gmail.com>
2024-12-31 17:43:35 +0900

    product entity 수정

--- src/main/java/com/joo/abysshop/entity/Product.java
+++ src/main/java/com/joo/abysshop/entity/Product.java
@@ -1,15 +1,13 @@
 package com.joo.abysshop.entity;
 
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Product {
     private int productId;
-    private String productname;
+    private String productName;
     private int price;
     private String description;
     private byte[] image;

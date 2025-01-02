5a58676a2e5a9d83577ff91222379e0daa71a893
bam <juhyun19981129@gmail.com>
2025-01-02 17:24:56 +0900

    Home 컨트롤러 Home 이동시 상품 목록을 불러오도록 변경

--- src/main/java/com/joo/abysshop/controller/HomeController.java
+++ src/main/java/com/joo/abysshop/controller/HomeController.java
@@ -1,13 +1,25 @@
 package com.joo.abysshop.controller;
 
+import com.joo.abysshop.dto.product.ProductListResponse;
+import com.joo.abysshop.enums.ProductType;
+import com.joo.abysshop.service.ProductService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final ProductService productService;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<ProductListResponse> productItemList = productService.findAllProducts(
+            ProductType.PRODUCT);
+        model.addAttribute("productItemList", productItemList);
         return "index";
     }
 }

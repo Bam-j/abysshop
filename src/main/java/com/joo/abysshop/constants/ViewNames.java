e3bf0471e42523050ad2e6022957456f6a21d8ea
bam <juhyun19981129@gmail.com>
2025-02-01 15:38:57 +0900

    ViewNames 상수 추가 정의와 수정

--- src/main/java/com/joo/abysshop/constants/ViewNames.java
+++ src/main/java/com/joo/abysshop/constants/ViewNames.java
@@ -6,23 +6,26 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ViewNames {
 
-    public static final String HOME = "/";
+    //home & index
+    public static final String HOME_PAGE = "/";
+    public static final String INDEX_PAGE = "index";
 
     //account
-    public static final String SIGN_IN = "account/signIn";
-    public static final String SIGN_UP = "account/signUp";
+    public static final String SIGN_IN_PAGE = "account/signIn";
+    public static final String SIGN_UP_PAGE = "account/signUp";
 
     //admin
+    public static final String ADMIN_MY_PAGE = "admin/adminMyPage";
 
     //cart
-
-    //common
+    public static final String SHOPPING_CART_PAGE = "cart/shoppingCart";
 
     //order
-
-    //point
+    public static final String ORDER_COMPLETE_PAGE = "order/orderComplete";
 
     //product
+    public static final String PRODUCT_DETAIL_PAGE = "product/productDetail";
 
     //user
+    public static final String USER_MY_PAGE = "user/userMyPage";
 }

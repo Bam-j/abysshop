fc6e5366a8de40238c91bdc8b9535d1dfd071aac
bam <juhyun19981129@gmail.com>
2025-02-01 15:09:31 +0900

    ViewNames 상수 정의

--- src/main/java/com/joo/abysshop/constants/ViewNames.java
+++ src/main/java/com/joo/abysshop/constants/ViewNames.java
@@ -1,5 +1,28 @@
 package com.joo.abysshop.constants;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ViewNames {
 
+    public static final String HOME = "/";
+
+    //account
+    public static final String SIGN_IN = "account/signIn";
+    public static final String SIGN_UP = "account/signUp";
+
+    //admin
+
+    //cart
+
+    //common
+
+    //order
+
+    //point
+
+    //product
+
+    //user
 }

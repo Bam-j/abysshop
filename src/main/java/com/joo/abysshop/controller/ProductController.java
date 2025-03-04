5ef4055afda534ff5965b0c2487119fd653efdfd
bam <juhyun19981129@gmail.com>
2025-03-04 14:57:08 +0900

    상품 상세 페이지에서 장바구니 정보가 표시되지 않던 문제 해결

--- src/main/java/com/joo/abysshop/controller/ProductController.java
+++ src/main/java/com/joo/abysshop/controller/ProductController.java
@@ -1,8 +1,12 @@
 package com.joo.abysshop.controller;
 
+import com.joo.abysshop.dto.cart.CartResponse;
+import com.joo.abysshop.dto.user.UserInfoResponse;
+import com.joo.abysshop.service.cart.CartService;
 import com.joo.abysshop.util.constants.ModelAttributeNames;
 import com.joo.abysshop.util.constants.ViewNames;
 import com.joo.abysshop.service.product.ProductService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,9 +18,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class ProductController {
 
     private final ProductService productService;
+    private final CartService cartService;
 
     @GetMapping("/product/detail/{productId}")
-    public String getProductDetail(@PathVariable Long productId, Model model) {
+    public String getProductDetail(@PathVariable Long productId, HttpSession session, Model model) {
+        UserInfoResponse user = (UserInfoResponse) session.getAttribute("user");
+        if (user != null) {
+            CartResponse cart = cartService.getCart(user.getUserId());
+            model.addAttribute(ModelAttributeNames.CART, cart);
+        }
+
         model.addAttribute(ModelAttributeNames.PRODUCT, productService.findById(productId));
         return ViewNames.PRODUCT_DETAIL_PAGE;
     }
